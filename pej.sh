43671a612b806d98f54885504b8a7fcbe7b3c44d
Wilker Aziz <wilker.aziz@gmail.com>
2013-05-09 01:08:50 +0100

    Supporting build with maven2 (nbproject is still supported)

--- pej.sh
+++ pej.sh
@@ -1,1 +1,1 @@
-java -Xms256m -Xmx512m -Dfile.encoding=UTF-8 -cp dist/PET.jar pet.frontend.PEJMain $@
+java -Xms256m -Xmx512m -Dfile.encoding=UTF-8 -cp target/PET-2.0-jar-with-dependencies.jar pet.frontend.PEJMain $@
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -Xms256m -Xmx512m -Dfile.encoding=UTF-8 -jar dist/PET.jar $@
+java -Xms256m -Xmx512m -Dfile.encoding=UTF-8 -cp target/PET-2.0-jar-with-dependencies.jar pet.frontend.PETMain $@
